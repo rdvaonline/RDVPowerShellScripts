e9a42d0865f6731e4801e488a80d48afbecb2417
Rob VanHoorne <rvanhoorne@icloud.com>
2016-05-31 12:57:15 -0500

    Added notifyByEmail call. Commented out production receipient code.

--- findStaleExternalPasswords.ps1
+++ findStaleExternalPasswords.ps1
@@ -9,12 +9,12 @@ function log ($string) {
 	$string | out-file -Filepath $logfile -append
 }
 
-function notifyByEmail ($recipient, $subject, $body) {
+function notifyByEmail ($recipient, $subject, $body, $attachmentPath) {
     # Set the PowerShell email server to our SMTP relay on app4. Set variables for use in the send-mailmessage command.
     $psemailserver = "app4"
     $sender = "helpdesk@cellulardynamics.com"
     
-    send-mailmessage -from $sender -to $recipient -subject $subject -body $body
+    send-mailmessage -from $sender -to $recipient -subject $subject -body $body -Attachments $attachmentPath
 }
 
 function daysRemainingUntilPasswordMustBeChanged {
@@ -25,23 +25,29 @@ function daysRemainingUntilPasswordMustBeChanged {
 		$currentDate= Get-Date 
 		$daysRemaining= $lastSetDate -$currentDate
 
-        $recipient = "$($SingleUser.samaccountname)@cellulardynamics.com"
-        $recipient += ",helpdesk@cellulardynamics.com"
-
-        log $recipient
+        # $recipient = "$($SingleUser.samaccountname)@cellulardynamics.com"
+        $recipient = "rob.vanhoorne.contractor@cellulardynamics.com"
+        $attachmentPath = "C:\Users\rvanhoorne\Desktop\How to Change Password for External Users.pdf"
 
         log "$(get-date)`t$($SingleUser.displayname)`t$($SingleUser.office)`t$daysRemaining"
 
-		if ($daysRemaining.Days -lt 0) {
-        
-		} 
+        if ($daysRemaining.Days -lt 7) {
+            # $recipient += ",helpdesk@cellulardynamics.com"
+            $subject = "Password Expires in $($daysRemaining.Days) Days for $($SingleUser.samaccountname)"
+            $body = "Hello! Your password will expire in $($daysRemaining.days) days. The helpdesk has been CC'd which will create a case for us to follow up with you. Step by step instructions for updating your password are attached."
 
-        if ($daysRemaining.Days -gt 0 -and $daysRemaining.Days -lt 7) {
-  
+            #log $recipient,$subject,$body,$attachmentPath,$daysRemaining.Days
+
+            notifyByEmail $recipient $subject $body $attachmentPath
         } 
         
         if ($daysRemaining.Days -gt 7 -and $daysRemaining.Days -lt 14) {
+            $subject = "Step by Step Instructions for Updating Your Password"
+            $body = "Hello! Your password will expire in $($daysRemaining.Days) days. Attached are step by step instructions on how to connect to the VPN and update your password. Please contact us at helpdesk@cellulardynamics.com if you have questions or need assistance."
 
+            #log $recipient,$subject,$body,$attachmentPath,$daysRemaining.Days
+
+            notifyByEmail $recipient $subject $body $attachmentPath
         }
 	}
 }
