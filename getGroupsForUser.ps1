1690ae9185fd4b37ce15c79a2090b7c0257f8f97
Rob VanHoorne <rvanhoorne@icloud.com>
2016-07-26 11:28:47 -0500

    Added email function. Removed deprecated methods. Updated OK and Cancel event handling.

--- getGroupsForUser.ps1
+++ getGroupsForUser.ps1
@@ -1,58 +1,70 @@
-﻿[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
 
-$objForm = New-Object System.Windows.Forms.Form
-$objForm.Text = "Data Entry Form"
-$objForm.Size = New-Object System.Drawing.Size(300,220)
-$objForm.StartPosition = "CenterScreen"
-
-$objForm.KeyPreview = $true
-$objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") {$username=$objTextBoxUsername.Text;$email=$objTextBoxEmail;$objForm.Close()}})
-$objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") {$objForm.Close()}})
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Email CSV of Employee Groups Form"
+$form.Size = New-Object System.Drawing.Size(300,220)
+$form.StartPosition = "CenterScreen"
 
 $OKButton = New-Object System.Windows.Forms.Button
 $OKButton.Location = New-Object System.Drawing.Size(75,140)
 $OKButton.Size = New-Object System.Drawing.Size(75,23)
 $OKButton.Text = "OK"
-$OKButton.Add_Click({$username=$objTextBoxUsername.Text;$email=$objTextBoxEmail;$objForm.Close()})
-$objForm.Controls.Add($OKButton)
+$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $OKButton
+$form.Controls.Add($OKButton)
 
 $CancelButton = New-Object System.Windows.Forms.Button
 $CancelButton.Location = New-Object System.Drawing.Size(150,140)
 $CancelButton.Size = New-Object System.Drawing.Size(75,23)
 $CancelButton.Text = "Cancel"
-$CancelButton.Add_Click({$objForm.Close()})
-$objForm.Controls.Add($CancelButton)
+$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $CancelButton
+$form.Controls.Add($CancelButton)
 
-$objLabelUsername = New-Object System.Windows.Forms.Label
-$objLabelUsername.Location = New-Object System.Drawing.Size(10,20)
-$objLabelUsername.Size = New-Object System.Drawing.Size(280,20)
-$objLabelUsername.Text = "Please enter the employee's username:"
-$objForm.Controls.Add($objLabelUsername)
+$labelUsername = New-Object System.Windows.Forms.Label
+$labelUsername.Location = New-Object System.Drawing.Size(10,20)
+$labelUsername.Size = New-Object System.Drawing.Size(280,20)
+$labelUsername.Text = "Please enter the employee's username:"
+$form.Controls.Add($labelUsername)
 
-$objTextBoxUsername = New-Object System.Windows.Forms.TextBox
-$objTextBoxUsername.Location = New-Object System.Drawing.Size(10,40)
-$objTextBoxUsername.Size = New-Object System.Drawing.Size(260,20)
-$objForm.Controls.Add($objTextBoxUsername)
+$textBoxUsername = New-Object System.Windows.Forms.TextBox
+$textBoxUsername.Location = New-Object System.Drawing.Size(10,40)
+$textBoxUsername.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBoxUsername)
 
-$objLabelEmail = New-Object System.Windows.Forms.Label
-$objLabelEmail.Location = New-Object System.Drawing.Size(10,80)
-$objLabelEmail.Size = New-Object System.Drawing.Size(280,20)
-$objLabelEmail.Text = "Please enter your email address:"
-$objForm.Controls.Add($objLabelEmail)
+$labelEmail = New-Object System.Windows.Forms.Label
+$labelEmail.Location = New-Object System.Drawing.Size(10,80)
+$labelEmail.Size = New-Object System.Drawing.Size(280,20)
+$labelEmail.Text = "Please enter your email address:"
+$form.Controls.Add($labelEmail)
 
-$objTextBoxEmail = New-Object System.Windows.Forms.TextBox
-$objTextBoxEmail.Location = New-Object System.Drawing.Size(10,100)
-$objTextBoxEmail.Size = New-Object System.Drawing.Size(260,20)
-$objForm.Controls.Add($objTextBoxEmail)
+$textBoxEmail = New-Object System.Windows.Forms.TextBox
+$textBoxEmail.Location = New-Object System.Drawing.Size(10,100)
+$textBoxEmail.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBoxEmail)
 
-$objForm.Topmost = $true
+$form.Topmost = $true
 
-$objForm.Add_Shown({$objForm.Activate(); $objTextBoxUsername.focus()})
-[void] $objForm.ShowDialog()
+$form.Add_Shown({$form.Activate(); $textBoxUsername.focus()})
+$result = $form.ShowDialog()
 
-$username = $objTextBoxUsername.Text
-$email = $objTextBoxEmail.Text
+function notifyByEmail ($emailAddress) {
+    # Set the PowerShell email server to our SMTP relay on app4. Set variables for use in the send-mailmessage command.
+    $psemailserver = "app4"
+    $sender = "helpdesk@cellulardynamics.com"
+    $recipient = $emailAddress
+    $subject = "Test!"
+    $body = "Test!"
+    
+    send-mailmessage -from $sender -to $recipient -subject $subject -body $body
+}
 
-$username
-$email
+if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+    $username = $textBoxUsername.Text
+    $email = $textBoxEmail.Text
+    $username
+    $email
+
+    notifyByEmail $email
+}
