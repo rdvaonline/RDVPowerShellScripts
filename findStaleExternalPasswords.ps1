3d161b1acac0c5313f0010d2ba3b162b34b5cd2b
Rob VanHoorne <rvanhoorne@icloud.com>
2016-08-08 09:51:22 -0500

    Resolved bug with sender in findStaleExternalPasswords

--- findStaleExternalPasswords.ps1
+++ findStaleExternalPasswords.ps1
@@ -1,7 +1,7 @@
-$logfile = "C:\homeofficeover90_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
+$logfile = "\\fsdc\Scripts\logs\findStaleExternalPasswords_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
 
 function main() {
-	daysSincePasswordLastSet
+	daysRemainingUntilPasswordMustBeChanged
 }
 
 function log ($string) {
@@ -9,17 +9,43 @@ function log ($string) {
 	$string | out-file -Filepath $logfile -append
 }
 
-function daysSincePasswordLastSet {
-	$HomeOfficeUser=Get-ADUser -properties displayname,pwdlastset,mail,office -filter {(Enabled -eq "True") -and (Office -eq "Home Office")}
+function notifyByEmail ($sender, $recipient, $subject, $body, $attachmentPath) {
+    # Set the PowerShell email server to our SMTP relay on app4. Set variables for use in the send-mailmessage command.
+    $psemailserver = "app4"
+    
+    send-mailmessage -from $sender -to $recipient -subject $subject -body $body -Attachments $attachmentPath
+}
+
+function daysRemainingUntilPasswordMustBeChanged {
+	$HomeOfficeUser=Get-ADUser -properties displayname,samaccountname,pwdlastset,mail,office -filter {(Enabled -eq "True") -and (Office -eq "Home Office")}
 	
 	foreach($SingleUser in $HomeOfficeUser) {
-		$FirstDate=[datetime]::FromFileTime($SingleUser.pwdlastset) 
-		$SecondDate= Get-Date 
-		$Result= $SecondDate -$FirstDate 
+		$lastSetDate=[datetime]::FromFileTime($SingleUser.pwdlastset) 
+		$currentDate= Get-Date 
+		$daysRemaining= $lastSetDate -$currentDate
 
-		if ($Result.Days -gt 90) {
-			log "$(get-date)`t$($SingleUser.displayname)`t$($SingleUser.office)`t$Result"
-		} 
+        $recipient = @("$($SingleUser.samaccountname)@cellulardynamics.com")
+        $sender = $recipient
+        $attachmentPath = "\\fsdc\Scripts\How to Change Password for External Users.pdf"
+
+        log "$(get-date)`t$($SingleUser.displayname)`t$($SingleUser.office)`t$daysRemaining"
+
+        if ($daysRemaining.Days -lt 7) {
+            $recipient += "helpdesk@cellulardynamics.com"
+            $subject = "Password Expires in $($daysRemaining.Days) Days for $($SingleUser.samaccountname)"
+            $body = "Hello! Your password will expire in $($daysRemaining.days) days. The helpdesk has been CC'd which will create a case for us to follow up with you. Step by step instructions for updating your password are attached."
+
+            #notifyByEmail $sendor $recipient $subject $body $attachmentPath
+            log $recipient
+        } 
+        
+        if ($daysRemaining.Days -gt 7 -and $daysRemaining.Days -lt 14) {
+            $subject = "Step by Step Instructions for Updating Your Password"
+            $body = "Hello! Your password will expire in $($daysRemaining.Days) days. Attached are step by step instructions on how to connect to the VPN and update your password. Please contact us at helpdesk@cellulardynamics.com if you have questions or need assistance."
+
+            #notifyByEmail $sendor $recipient $subject $body $attachmentPath
+            log $recipient
+        }
 	}
 }
 
--- officePropertyNotSet.ps1
+++ officePropertyNotSet.ps1
@@ -1,29 +1,34 @@
-# Finds Active Directory users where the Office property is not set, logs the result to a text file, and notifies administrators via email.
-
-$logfile = "C:\officePropertyNotSet_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
+# Finds Active Directory users where the Office property is not set, writes results to a .CSV, and emails .CSV to an administrator.
 
 function main() {
 	officePropertyNotSet
 }
 
-function log ($string) {
-	write-host "$(get-date) $string"
-	$string | out-file -Filepath $logfile -append
+function notifyByEmail ($attachmentPath) {
+    # Set the PowerShell email server to our SMTP relay on app4. Set variables for use in the send-mailmessage command.
+    $psemailserver = "app4"
+    $sender = "helpdesk@cellulardynamics.com"
+    $recipient = "swolfe@cellulardynamics.com"
+    $subject = "List of Users Who Do Not Have the Office Property Set in ADUC"
+    $body = "Attached is a list of users who do not have the Office property set. Please set the property in Active Directory Users and Computers."
+    
+    send-mailmessage -from $sender -to $recipient -subject $subject -body $body -Attachments $attachmentPath
 }
 
 function officePropertyNotSet {
+    # Set variables for OUs to be excluded by the search. These OUs contain external users that are not employees of CDI, service accounts, and other administrative resources.
 	$utilityOU = "*OU=Utility,OU=Users,OU=CDI-ScienceDr,DC=cdi,DC=local"
 	$serviceAccountOU = "*OU=Service Accounts,OU=Users,OU=CDI-ScienceDr,DC=cdi,DC=local"
 	$monitoringMailboxes = "*CN=Monitoring Mailboxes,CN=Microsoft Exchange System Objects,DC=cdi,DC=local"
 	$vendorsOU = "*OU=Vendors,OU=Users,OU=CDI-ScienceDr,DC=cdi,DC=local"
 	$mailContactsOU = "*OU=Mail Contacts,OU=Users,OU=CDI-ScienceDr,DC=cdi,DC=local"
 	
-
+  
 	$OfficePropertyNotSetUser=Get-ADUser -properties displayname,distinguishedName,office -filter {(Enabled -eq "True") -and (office -notlike "*")} | where-object {($_.DistinguishedName -notlike $utilityOU) -and ($_.DistinguishedName -notlike $serviceAccountOU) -and ($_.DistinguishedName -notlike $monitoringMailboxes) -and ($_.DistinguishedName -notlike $vendorsOU) -and ($_.DistinguishedName -notlike $mailContactsOU)}
-	
-	foreach ($SingleUser in $OfficePropertyNotSetUser) {
-		log "$($SingleUser.displayname)`t $($SingleUser.distinguishedName)"
-	}
+    $attachmentPath = "\\fsdc\Scripts\logs\officePropertyNotSet_$(get-date -format `"yyyyMMdd_hhmmsstt`").csv"
+    $OfficePropertyNotSetUser | select DisplayName,Office, DistinguishedName,Enabled | export-csv $attachmentPath
+
+    notifyByEmail $attachmentPath
 }
 
 main
