980185968cdbc396bcfc802edf47289efa2961a1
Rob VanHoorne <rvanhoorne@icloud.com>
2016-08-26 13:02:24 -0500

    Resolve issue with getGroupsForUser Display Name lookup.

---
+++ uninstallTrendInstallMcAfee.ps1
@@ -0,0 +1,20 @@
+﻿$logfile = "\\fsdc\Scripts\logs\uninstallTrendInstallMcAfee_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
+
+function main() {
+	checkForTrendInstall
+}
+
+function log ($string) {
+	write-host "$string"
+	$string | out-file -Filepath $logfile -append
+}
+
+function checkForTrendInstall {
+    try {
+        $isTrendInstalled = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -like "*Trend*"} | Select-Object DisplayName
+    } catch {
+        log "Trend Install Not Found"
+    }
+}
+
+main
